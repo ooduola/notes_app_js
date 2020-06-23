3da021c752d4b7ae56aa3224b252fa26fd7bef1e
Sumner1185 <alastair.sumner@yahoo.com>
2020-06-23 16:05:52 +0100

    Add conditional that throws error if no notes to return

--- spec/note-list-view-test.js
+++ spec/note-list-view-test.js
@@ -34,14 +34,7 @@
     var noteListView = new NoteListView();
     noteListView.list.createNote("Hello");
     noteListView.list.createNote("Test");
-
-    var array = noteListView.list.notes
   
-
-    console.log("hello")
-    console.log(array.join("</div></li><li><div>"))
-    console.log(noteListView.returnString())
-
     if(noteListView.returnString() !== '<ul><li><div>Hello</div></li><li><div>Test</div></li></ul>') {
       throw new Error('Nah mateeee');
     }
--- src/note-list-view.js
+++ src/note-list-view.js
@@ -6,17 +6,13 @@
 
     NoteListView.prototype.returnString = function() {
       var array = this.list.notes
-
-      return "<ul><li><div>" + array.join("</div></li><li><div>") + "</div></li></ul>"
-      // `<ul><li><div>${array[0]}</div></li></ul>`
+      if(array.length < 1) {
+        throw new Error ("Notes list empty")
+      } else {
+        return "<ul><li><div>" + array.join("</div></li><li><div>") + "</div></li></ul>"
+      }
     };
 
   exports.NoteListView = NoteListView;
 
 })(this);
-
-
-// var i;
-// for (i = 0; i < cars.length; i++) {
-//   text += cars[i] + "<br>";
-// }
