9a79c5264aef58615b4e9d8b542841eb13e20f1b
Olatunji Oduola <ola.oduola@gmail.com>
2020-06-23 11:32:24 +0100

    testing without library playaround complete

---
+++ test_circle/circle-tests.js
@@ -0,0 +1,9 @@
+// (function(exports) {
+  function testCircleRadiusDefaultsTo10() {
+    var circle = new Circle();
+
+    assert.isTrue(circle.radius === 10);
+  };
+
+//   testCircleRadiusDefaultsTo10();
+// })(this);
