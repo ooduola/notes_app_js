2b316c6ed3a03b7deaa5d4e769faea84e5299faf
Olatunji Oduola <ola.oduola@gmail.com>
2020-06-23 15:56:49 +0100

    Test now works for severals notes

--- spec/note-list-view-test.js
+++ spec/note-list-view-test.js
@@ -1,3 +1,5 @@
+// import { NoteListView } from "../src/note-list-view";
+
 (function (exports) {
 
   function testNoteListViewHasNoteList() {
@@ -23,4 +25,27 @@
   }
 
   testReturnsHTMLString();
+})(this);
+
+(function (exports) {
+
+  function testReturnsHTMLStringWithSeveralNotes() {
+
+    var noteListView = new NoteListView();
+    noteListView.list.createNote("Hello");
+    noteListView.list.createNote("Test");
+
+    var array = noteListView.list.notes
+  
+
+    console.log("hello")
+    console.log(array.join("</div></li><li><div>"))
+    console.log(noteListView.returnString())
+
+    if(noteListView.returnString() !== '<ul><li><div>Hello</div></li><li><div>Test</div></li></ul>') {
+      throw new Error('Nah mateeee');
+    }
+  }
+
+  testReturnsHTMLStringWithSeveralNotes();
 })(this);
--- src/note-list-view.js
+++ src/note-list-view.js
@@ -6,9 +6,17 @@
 
     NoteListView.prototype.returnString = function() {
       var array = this.list.notes
-      return `<ul><li><div>${array[0]}</div></li></ul>`
+
+      return "<ul><li><div>" + array.join("</div></li><li><div>") + "</div></li></ul>"
+      // `<ul><li><div>${array[0]}</div></li></ul>`
     };
 
   exports.NoteListView = NoteListView;
 
 })(this);
+
+
+// var i;
+// for (i = 0; i < cars.length; i++) {
+//   text += cars[i] + "<br>";
+// }
