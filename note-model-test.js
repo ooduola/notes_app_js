989de33dba5eec7bc9e92ae3c02381f24eafa1f4
Olatunji Oduola <ola.oduola@gmail.com>
2020-06-23 13:15:56 +0100

    Refactored Test to include assert.js

--- note-model-test.js
+++ note-model-test.js
@@ -1,11 +1,15 @@
-(function(exports) {
+// (function(exports) {
 
   function checkForNote() {
-    var note1 = new NotesApp();
+    var note1 = new NotesApp("My favourite language is JavaScript");
 
-    if (note1.note !== "My favourite language is JavaScript") {
-      throw new Error("Note does not match My favourite language is JavaScript")
-    }
+    assert.isTrue(note1.note === "My favourite language is JavaScript")
+
   };
-  checkForNote();
-})(this);
+
+//     if (note1.note !== "My favourite language is JavaScript") {
+//       throw new Error("Note does not match My favourite language is JavaScript")
+//     }
+//   };
+//   checkForNote();
+// })(this);
